ad3611d1fd35dfe184da127e24fee6554420f75a
chenss <hn2cyy@163.com>
2016-01-19 19:15:05 +0800

    add file :
    	router.js
    	site.js

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var config = require('./config');
 var path = require('path');
 var app = express();
 var staticDir = path.join(__dirname,'public');
+var router = require('./router.js');
 
 app.use(compress());
 
 app.use('/public',express.static(staticDir));
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','html');
+app.engine('html',require('ejs-mate'));
+app.locals._layoutFile = 'layout.html';
+app.enable('trust proxy');
 
-app.get('/',function(err,req,res){
-
-})
-
+app.use(/,router);
 app.listen(config.port,function(){
   console.log('server start:'+config.port);
 })
